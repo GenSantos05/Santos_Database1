f432ced9cc4f71706fc88c48cbea8651a02eaa6a
kestersanoyyy <123294218+GenSantos05@users.noreply.github.com>
2023-02-06 18:38:58 +0800

    Database Activity 1
    This is my database file

---
+++ database.php
@@ -0,0 +1,22 @@
+<?php
+$servername = "localhost";
+$username = "GenielynSantos";
+$password = "Santos12345";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// Create database
+$sql = "CREATE DATABASE santos";
+if ($conn->query($sql) === TRUE) {
+  echo "Database created successfully";
+} else {
+  echo "Error creating database: " . $conn->error;
+}
+
+$conn->close();
+?>
